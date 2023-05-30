f348cf97a603885b4dcad99a18d32ec0776f3bba
Brendan McKenzie <hello@brendanmckenzie.com>
2023-05-30 11:23:42 +1000

    hmm

--- res/workout.js
+++ res/workout.js
@@ -2,14 +2,14 @@ let wakeLock = null;
 
 const requestWakeLock = async () => {
   try {
-    wakeLock = await navigator.wakeLock.request();
+    wakeLock = await navigator.wakeLock.request("screen");
     wakeLock.addEventListener("release", () => {
       console.log("Screen Wake Lock released:", wakeLock.released);
     });
     console.log("Screen Wake Lock released:", wakeLock.released);
   } catch (err) {
+    // TODO: fix this. NotAllowedError, Permission was denied
     console.error(`${err.name}, ${err.message}`);
   }
 };
-
 requestWakeLock();
